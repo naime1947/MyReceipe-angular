2a5f523ab7e8ba1f92615de888b9375eb59df788
Naime Ahmed <naime1947@gmail.com>
2020-01-28 12:00:52 +0600

    receipe model creaed and recipe list generated

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes:Recipe[]=[
+    new Recipe('Kola Recipe','Very good colla recipe', 'https://cdn.pixabay.com/photo/2017/06/27/22/21/banana-2449019_960_720.jpg' ),
+  new Recipe('Jambura Recipe','Very good Jambura recipe', 'https://www.agomonbd.com/wp-content/uploads/2019/09/51eSK3PFBKL.png' ),
+  new Recipe('Mengo Recipe','Very good colla recipe', 'https://i.pinimg.com/474x/99/15/0b/99150b2c10c4200b4c9051cfdd469a82.jpg' )
+];
 
   constructor() { }
 
