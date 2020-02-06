b5b960af10f19bb28c57a872e258e0ab1453ce35
Naime Ahmed <naime1947@gmail.com>
2020-02-06 06:53:42 +0600

    Recipe and shoping list menu  navigation managed using component interection and ng if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyReceipe-angular';
+
+  selectedMenu:string;
+  onNavigate(selectedMenu:string){
+    this.selectedMenu = selectedMenu;
+  }
 }
